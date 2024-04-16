69063847e78aef8f636ad370c970765522fd7327
Nick Jugganaikloo <nick.jgniklu@gmail.com>
2024-04-15 21:04:28 -0600

    force static memory

--- src/tensorflow/lite/kernels/kernel_util.cc
+++ src/tensorflow/lite/kernels/kernel_util.cc
@@ -13,7 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 #include "tensorflow/lite/kernels/kernel_util.h"
-
+#define TF_LITE_STATIC_MEMORY
 #include <stdint.h>
 #include <stdlib.h>
 
